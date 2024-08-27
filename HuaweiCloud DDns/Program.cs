9340216623b23605048d77a80facf0a0d0865219
NavAI-B <1633182290@qq.com>
2024-08-27 22:14:23 +0800

    首次提交工程

---
+++ HuaweiCloud DDns/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceProcess;
+
+namespace HuaweiCloud_DDns
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new MainService()
+            };
+            ServiceBase.Run(ServicesToRun);
+
+        }
+    }
+}
